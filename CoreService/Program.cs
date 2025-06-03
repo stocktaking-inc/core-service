3797d9fac65f76e893bd7d181c6ceae27ddbc0dc
Daniil Oberlev <denioberlev@gmail.com>
2025-06-03 14:18:13 +0300

    feat(cors): add cors

--- CoreService/Program.cs
+++ CoreService/Program.cs
@@ -3,11 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+  options.ListenAnyIP(5101);
+});
+
 builder.Services.AddCors(options =>
 {
   options.AddPolicy("AllowFrontend", policy =>
   {
-    policy.WithOrigins("http://localhost:3001")
+    policy.WithOrigins(
+        "http://localhost:5173",  // react
+        "http://localhost:3000",  // next
+        "https://localhost:8443", // nginx
+        "http://localhost:3001"   // next test
+      )
       .AllowAnyHeader()
       .AllowAnyMethod();
   });
